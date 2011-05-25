bc4582cb1423258de76f43b28253c63b2342e637
Zhou Zhiwei <hotdog092@gmail.com>
2011-05-25 01:22:51 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AveventApp::Application.config.secret_token = 'e1930f5537ac0b6e4e223365b0b82e093750de56e3a2b05f0589553840d1a33ccce84fa0088d682a2f9d4269e765ea339fec2b394b39ae5465c5f77a8537cd75'
