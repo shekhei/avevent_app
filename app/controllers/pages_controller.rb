d0b984302a41f3aa1aeb6387c58b775be22a6ed9
Zhou Zhiwei <hotdog092@gmail.com>
2011-05-30 03:05:16 -0700

    Commit for today work end

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
-    @todayevents = Event.show_today
-
+    @today_events = Event.for_today
+    @week_events = Event.from_two_days_later.for_this_week
+    @tomorrow_events = Event.for_tomorrow
   end
 
   def about
--- app/models/event.rb
+++ app/models/event.rb
@@ -17,8 +17,10 @@ class Event < ActiveRecord::Base
   has_many :users, :through => :rsvps
   
   attr_accessible :name, :peak, :start_time, :end_time
+  scope :for_today, where(:start_time => Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+  scope :for_tomorrow, where(:start_time => Time.zone.now.tomorrow.beginning_of_day..Time.zone.now.tomorrow.end_of_day)
+  scope :for_this_week, where(:start_time => Time.zone.now.beginning_of_week..Time.zone.now.end_of_week)
+  scope :from_two_days_later, where("start_time > ?", Time.zone.now.midnight + 2.day)
+  scope :is_active, where("start_time > ?", Time.zone.now.midnight)
 
-  def self.show_today
-    where(:start_time => Time.now.midnight..Time.now.midnight + 1.day)
-  end
 end
