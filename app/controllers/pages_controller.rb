b86e4a0e27d134ff8772ab94adf60f9cdb0e8a21
Zhou Zhiwei <hotdog092@gmail.com>
2011-05-31 01:23:49 +0800

    fixing styles on event page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
     @title = "Home"
     @today_events = Event.for_today
-    @week_events = Event.from_two_days_later.for_this_week
     @tomorrow_events = Event.for_tomorrow
+    @week_events = Event.from_two_days_later.for_this_week
   end
 
   def about
